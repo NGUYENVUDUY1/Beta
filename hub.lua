8eb1f46f5586216e4e1c7e04cafc051feb6f0cda
NGUYENVUDUY1 <165898044+NGUYENVUDUY1@users.noreply.github.com>
2024-08-27 06:34:23 +0700

    Add files via upload

---
+++ hub.lua
@@ -0,0 +1,7 @@
+setclipboard("https://discord.gg/skX5U6Gm")
+local msg = [==[
+Join Discord For The New Update Script |
+Vào Discord Để Lấy Script mới nhất
+ | https://discord.gg/skX5U6Gm
+]==]
+game:GetService("Players").LocalPlayer:Kick(msg)
